1a34554bd6cdb7a71f8b26550521741e67d04749
= <=>
2016-01-16 21:34:01 +0100

    Add records

--- Users.elm
+++ Users.elm
@@ -6,7 +6,8 @@ import StartApp.Simple as StartApp
 import String
 
 type alias Customer = {name : String, message : String}
-type alias Model = { customers : List Customer }
+type alias Uistate = {fieldName : String, fieldMessage : String }
+type alias Model = { customers : List Customer, fieldName : String, fieldMessage : String}
 
 model : Model
 model = { customers = [
@@ -14,19 +15,49 @@ model = { customers = [
                 {name =  "Emil Haugberg van Veen", message = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."},
                 {name =  "Luca Verhees", message =  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."}
             ]
+          , fieldName = ""
+          , fieldMessage = ""
        }
 
 main =
   StartApp.start { model = model , update = update, view = view }
 
-type alias Action = String
-
 view : Signal.Address Action -> Model -> Html.Html
 view action model =
-  customers model.customers
+  div []
+    [customers model.customers
+    , input
+        [ placeholder "Name"
+        , on "input" targetValue (Signal.message action << SetName)
+        ]
+        []
+    ,input
+        [ placeholder "Message"
+        , on "input" targetValue (Signal.message action << SetMessage)
+        ]
+        []
+    , button [ onClick action Add ] [ text "add" ]
+    ]
 
+
+type Action = SetName String | SetMessage String | Add
 update : Action -> Model -> Model
-update _ y = y
+update action model =
+  case action of
+    Add ->
+      {model |
+        customers = model.customers ++ [newCustomer model.fieldName model.fieldMessage]
+        , fieldName = ""
+        , fieldMessage = ""
+      }
+    SetName str -> {model | fieldName = str}
+    SetMessage str -> {model | fieldMessage = str}
+
+newCustomer : String -> String -> Customer
+newCustomer name message =
+  { name = name
+  , message = message
+  }
 
 customers : List Customer -> Html
 customers customers =
