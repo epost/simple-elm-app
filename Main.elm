622044e109274ffbdd2b4bc0cb52f989e7c59af7
Erik Post <erik@shinsetsu.nl>
2016-01-18 12:11:40 +0100

    MOAR clenaup! rawr...

--- Main.elm
+++ Main.elm
@@ -5,7 +5,10 @@ import Signal exposing (..)
 import StartApp.Simple as StartApp
 
 type alias Customer = { id : Int, name : String, message : String }
+
 type alias Model    = { fieldName : String, fieldMessage : String, uid : Int, customers : List Customer}
+
+type Action = SetName String | SetMessage String | Delete Int | Add
 
 model : Model
 model = { fieldName = ""
@@ -18,49 +21,30 @@ model = { fieldName = ""
         }
 
 main : Signal Html
-main =
-  StartApp.start { model = model , update = update, view = view }
+main = StartApp.start { model = model, update = update, view = view }
 
 view : Signal.Address Action -> Model -> Html.Html
 view adr model =
   div []
     [ customersHtml model.customers adr
-    , input
-        [ placeholder "Name"
-        , on "input" targetValue (Signal.message adr << SetName)
-        ]
-        []
-    , input
-        [ placeholder "Message"
-        , on "input" targetValue (Signal.message adr << SetMessage)
-        ]
-        []
-    , button [ onClick adr Add ] [ text "add" ]
+    , input [placeholder "Name"   , on "input" targetValue (Signal.message adr << SetName   )] []
+    , input [placeholder "Message", on "input" targetValue (Signal.message adr << SetMessage)] []
+    , button [onClick adr Add] [text "add"]
     ]
 
-
-type Action = SetName String | SetMessage String | Delete Int | Add
-
 update : Action -> Model -> Model
-update action model =
-  case action of
-    Add ->
-      {model |
-        fieldName = ""
-        , fieldMessage = ""
-        , uid = model.uid + 1
-        , customers = model.customers ++ [newCustomer model.uid model.fieldName model.fieldMessage]
-      }
-    SetName str    -> { model | fieldName = str}
-    SetMessage str -> { model | fieldMessage = str}
-    Delete id      -> { model | customers = List.filter (\t -> t.id /= id) model.customers }
+update action model = case action of
+  Add -> { model | fieldName = ""
+                 , fieldMessage = ""
+                 , uid = model.uid + 1
+                 , customers = model.customers ++ [newCustomer model.uid model.fieldName model.fieldMessage]
+         }
+  SetName str    -> { model | fieldName = str }
+  SetMessage str -> { model | fieldMessage = str }
+  Delete id      -> { model | customers = List.filter (\t -> t.id /= id) model.customers }
 
-newCustomer : Int -> String -> String -> Customer
-newCustomer id name message =
-  { id = id
-  , name = name
-  , message = message
-  }
+mkCustomer : Int -> String -> String -> Customer
+mkCustomer id name message = { id = id, name = name, message = message }
 
 customersHtml : List Customer -> Address Action -> Html
 customersHtml customers adr =
@@ -68,12 +52,7 @@ customersHtml customers adr =
 
 customerHtml : Customer -> Address Action -> Html
 customerHtml customer adr =
-    div []
-      [ div [] [text customer.name]
-      , div [] [text customer.message]
-      , button
-            [ class "destroy"
-            , onClick adr (Delete customer.id)
-            ]
-            [ text "Delete" ]
-      ]
+    div [] [ div [] [text customer.name]
+           , div [] [text customer.message]
+           , button [class "destroy", onClick adr (Delete customer.id)] [text "Delete"]
+           ]
