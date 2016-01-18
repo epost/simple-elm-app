8442c58595f190aaa086d0af7f3e131efee6292e
= <=>
2016-01-18 11:39:31 +0100

    New version User.elm

--- Users.elm
+++ Users.elm
@@ -3,10 +3,9 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Signal exposing (..)
 import StartApp.Simple as StartApp
-import String
+-- import String
 
-type alias CustomerId = Int
-type alias Customer = { id : CustomerId, name : String, message : String}
+type alias Customer = { id : Int, name : String, message : String}
 type alias Uistate = {fieldName : String, fieldMessage : String }
 type alias Model = { fieldName : String, fieldMessage : String, uid : Int, customers : List Customer}
 
@@ -27,24 +26,25 @@ main =
   StartApp.start { model = model , update = update, view = view }
 
 view : Signal.Address Action -> Model -> Html.Html
-view action model =
+view adr model =
   div []
-    [customers model.customers
+    [ customersHtml model.customers adr
     , input
         [ placeholder "Name"
-        , on "input" targetValue (Signal.message action << SetName)
+        , on "input" targetValue (Signal.message adr << SetName)
         ]
         []
     ,input
         [ placeholder "Message"
-        , on "input" targetValue (Signal.message action << SetMessage)
+        , on "input" targetValue (Signal.message adr << SetMessage)
         ]
         []
-    , button [ onClick action Add ] [ text "add" ]
+    , button [ onClick adr Add ] [ text "add" ]
     ]
 
 
 type Action = SetName String | SetMessage String | Delete Int | Add
+
 update : Action -> Model -> Model
 update action model =
   case action of
@@ -55,29 +55,37 @@ update action model =
         , uid = model.uid + 1
         , customers = model.customers ++ [newCustomer model.uid model.fieldName model.fieldMessage]
       }
-    SetName str -> {model | fieldName = str}
-    SetMessage str -> {model | fieldMessage = str}
-    Delete id -> {model | customers = List.filter (\t -> t.id /= id) model.customers}
+    SetName str    -> { model | fieldName = str}
+    SetMessage str -> { model | fieldMessage = str}
+    Delete id      -> { model | customers = List.filter (\t -> t.id /= id) model.customers }
 
-newCustomer : CustomerId -> String -> String -> Customer
+newCustomer : Int -> String -> String -> Customer
 newCustomer id name message =
   { id = id
-  ,name = name
+  , name = name
   , message = message
   }
 
-customers : List Customer -> Html
-customers customers =
-  div [] (List.map customer customers )
+-- customersHtml : Address Action -> List Customer -> Html
+-- customersHtml adr customers =
+--   div [] (List.map (customerHtml adr) customers)
+customersHtml : List Customer -> Address Action -> Html
+customersHtml customers adr =
+  div [] (List.map ((flip customerHtml) adr) customers)
+-- customersHtml : List Customer -> Html
+-- customersHtml customers =
 
-customer : Address Action -> Customer -> Html
-customer action customer =
+-- customerHtml : Customer -> Html
+-- customerHtml customer =
+-- customerHtml : Address Action -> Customer -> Html
+-- customerHtml adr customer =
+customerHtml : Customer -> Address Action -> Html
+customerHtml customer adr =
     div []
-      [div [] [text customer.name]
+      [ div [] [text customer.name]
       , div [] [text customer.message]
       , button
-            [ class "destroy"
-            , onClick action (Delete customer.id)
+            [ onClick adr (Delete customer.id)
             ]
             [ text "Delete" ]
       ]
